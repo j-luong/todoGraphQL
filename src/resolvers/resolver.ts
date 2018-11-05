08177e0d167d5198eea60a7694ecaa6d5ba54f9e
Jason Luong <luongj02@mc-n357029.local>
2018-11-05 09:57:03 +0000

    added all crud actions

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -21,7 +21,35 @@ function getTodo(id: number): Todo[] {
   }]
 }
 
+function listTodos(): Todo[] {
+  const todos = Object.keys(db).map(todoId => db[todoId]);
+  return todos;
+}
+
+function newTodo(content: string): [Todo] {
+  const id = parseInt(Object.keys(db).pop(), 10) + 1;
+  const todo = createTodo(id, content);
+  db[todo.id] = todo;
+  return [todo];
+}
+
+function completeTodo(id: number): [Todo] {
+  const todo = db[id];
+  todo.done = true;
+  return [todo];
+}
+
+function deleteTodo(id: number): [] {
+  const todo = db[id];
+  if (todo) { delete db[id]; }
+  return [];
+}
+
 export const resolver = {
   greeting: ({ name = 'world' }) =>  `Hello ${name}`,
-  getTodo: ({ id }) =>  getTodo(id)
+  getTodo: ({ id }) =>  getTodo(id),
+  listTodos: () => listTodos(),
+  createTodo: ({ content }) => newTodo(content),
+  completeTodo: ({ id }) => completeTodo(id),
+  deleteTodo: ({ id }) => deleteTodo(id)
 };
