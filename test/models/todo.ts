e0887924f529f0b604b4d342ec0f887d4ed3e51c
Jason Luong <luongj02@mc-n357029.local>
2018-11-11 14:06:54 +0000

    added tests for resolvers

--- test/models/todo.ts
+++ test/models/todo.ts
@@ -1,15 +1,17 @@
 import { assert } from 'chai';
 import { createTodo } from '../../src/models/todo';
 
-describe('Todo', () => {
-  it('creates a todo', () =>{
-    const mockTodo = {
-      id: 1,
-      content: 'Hello world',
-      done: false
-    }
-
-    const todo = createTodo(1, 'Hello world');
-    assert.deepEqual(todo, mockTodo);
-  })
+describe('Models', () => {
+  describe('Todo', () => {
+    it('creates a todo', () =>{
+      const mockTodo = {
+        id: 1,
+        content: 'Hello world',
+        done: false
+      }
+  
+      const todo = createTodo(1, 'Hello world');
+      assert.deepEqual(todo, mockTodo);
+    });
+  });
 });
--- test/resolvers/resolver.ts
+++ test/resolvers/resolver.ts
@@ -3,7 +3,7 @@ import { resolver }  from '../../src/resolvers/resolver';
 
 let db = {};
 
-describe('Resolver', () => {
+describe('Resolvers', () => {
   beforeEach(() => {
     ((): void => {
       const todos = ['Finish todoGQL', 'Complete OKRs', 'Learn functional programming'];
@@ -41,6 +41,48 @@ describe('Resolver', () => {
         todo = err;
       }
       assert.instanceOf(todo, Error);
+    });
+  });
+  
+  describe('#listTodo', () => {
+    it('gets all todos', () =>{
+      const todos = resolver.listTodos();
+      const dbTodos = Object.keys(db);
+
+      assert.isArray(todos);
+      assert.equal(todos.length, dbTodos.length);
+    });
+  });
+
+  describe('#createTodo', () => {
+    it('creates a new todo', () => {
+      const todo = resolver.createTodo({ content: "Hello world" });
+      assert.equal(todo[0].content, "Hello world");
+    });
+  });
+
+  describe('#completeTodo', () => {
+    it('completes a todo', () => {
+      resolver.completeTodo({ id: 1 });
+      const todo = resolver.getTodo({ id: 1 });
+      assert.equal(todo[0].done, true);
+    });
+
+    it('throws if todo does not exist', () => {
+      let todo;
+      try {
+        resolver.completeTodo({ id: 919420148 });
+      } catch (err) {
+        todo = err;
+      }
+      assert.instanceOf(todo, Error);
+    })
+  });
+
+  xdescribe('#deleteTodo', () => {
+    it('deletes a todo', () => {
+      resolver.deleteTodo({ id: 1 });
+      // assert.notExists(db[0]);
     });
   });
 });
